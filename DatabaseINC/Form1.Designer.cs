a4fb10ed180f7499082f759e89071a6fcfc341d4
Cory Burns <cburns12@students.towson.edu>
2025-03-19 13:11:08 -0500

    Add project files.

---
+++ DatabaseINC/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace DatabaseINC
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Connect = new Button();
+            CreateDatabase = new Button();
+            CreateTable = new Button();
+            SuspendLayout();
+            // 
+            // Connect
+            // 
+            Connect.Location = new Point(301, 46);
+            Connect.Name = "Connect";
+            Connect.Size = new Size(200, 40);
+            Connect.TabIndex = 0;
+            Connect.Text = "Connect";
+            Connect.UseVisualStyleBackColor = true;
+            Connect.Click += Connect_Click;
+            // 
+            // CreateDatabase
+            // 
+            CreateDatabase.Location = new Point(301, 103);
+            CreateDatabase.Name = "CreateDatabase";
+            CreateDatabase.Size = new Size(200, 40);
+            CreateDatabase.TabIndex = 1;
+            CreateDatabase.Text = "Create Database";
+            CreateDatabase.UseVisualStyleBackColor = true;
+            CreateDatabase.Click += CreateDatabase_Click;
+            // 
+            // CreateTable
+            // 
+            CreateTable.Location = new Point(301, 161);
+            CreateTable.Name = "CreateTable";
+            CreateTable.Size = new Size(200, 40);
+            CreateTable.TabIndex = 2;
+            CreateTable.Text = "Create Table";
+            CreateTable.UseVisualStyleBackColor = true;
+            CreateTable.Click += CreateTable_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(CreateTable);
+            Controls.Add(CreateDatabase);
+            Controls.Add(Connect);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button Connect;
+        private Button CreateDatabase;
+        private Button CreateTable;
+    }
+}
---
+++ DatabaseINC/Form1.cs
@@ -0,0 +1,217 @@
+using MySql;
+using MySql.Data.MySqlClient;
+
+namespace DatabaseINC
+{
+    public partial class Form1 : Form
+    {
+        private MySqlConnection mainconnection;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Connect_Click(object sender, EventArgs e)
+        {
+            HideButtons();
+            TextBox _SERVER = new()
+            {
+                PlaceholderText = "Server",
+                Location = new(271, 45),
+                Width = 200,
+                ForeColor = Color.Red
+            };
+
+            TextBox _UID = new()
+            {
+                PlaceholderText = "Username",
+                Location = new(271, 91),
+                Width = 200,
+                ForeColor = Color.Red
+            };
+
+            TextBox _PW = new()
+            {
+                PlaceholderText = "Password",
+                Location = new(271, 137),
+                Width = 200,
+                ForeColor = Color.Red,
+                PasswordChar = '*'
+            };
+
+            Button connectMYSQL = new()
+            {
+                Text = "Connect",
+                Location = new(372, 183),
+                Width = 100
+            };
+
+            connectMYSQL.Click += (sender, e) =>
+            {
+                string connectionquery = $"server={_SERVER.Text};uid={_UID.Text};pwd={_PW.Text}";
+
+                while (_SERVER.Text == null || _UID.Text == null || _PW.Text == null)
+                {
+                    MessageBox.Show("One or more empty fields");
+                    return;
+                }
+
+                try
+                {
+                    mainconnection = new MySqlConnection(connectionquery);
+                    mainconnection.Open();
+                    MessageBox.Show("Connection established!");
+                }
+                catch (MySqlException ex)
+                {
+                    MessageBox.Show($"ERROR CONNECTING. {ex.Message}");
+                }
+            };
+
+            Button backbutton = new()
+            {
+                Text = "Back",
+                Location = new(271, 183),
+                Width = 100
+            };
+
+            backbutton.Click += (sender, e) =>
+            {
+                _SERVER.Visible = false;
+                _UID.Visible = false;
+                _PW.Visible = false;
+
+                Connect.Visible = true;
+                backbutton.Visible = false;
+                connectMYSQL.Visible = false;
+                CreateDatabase.Visible = true;
+                CreateTable.Visible = true;
+            };
+
+            this.Controls.Add(_SERVER);
+            this.Controls.Add(_UID);
+            this.Controls.Add(_PW);
+            this.Controls.Add(backbutton);
+            this.Controls.Add(connectMYSQL);
+        }
+
+        private void HideButtons()
+        {
+            Connect.Visible = false;
+            CreateDatabase.Visible = false;
+            CreateTable.Visible = false;
+        }
+
+        private void CreateDatabase_Click(object sender, EventArgs e)
+        {
+            // Checks if connection is null (which it always will be on first run)
+            while (mainconnection == null)
+            {
+                MessageBox.Show("Connection has not been established. Connect to MYSQL using your login information first.");
+                return;
+            }
+
+            HideButtons();
+            TextBox _DATABASE = new()
+            {
+                PlaceholderText = "Database Name",
+                Location = new(271, 45),
+                Width = 200,
+                ForeColor = Color.Red
+            };
+
+            Button CDBbackbutton = new()
+            {
+                Text = "Back",
+                Location = new(271, 183),
+                Width = 100
+            };
+
+            Button createdatabase = new()
+            {
+                Text = "Create",
+                Location = new(372, 183),
+                Width = 100
+            };
+
+            createdatabase.Click += (sender, e) =>
+            {
+                MySqlCommand CDBCommand = new();
+                string cdbname = _DATABASE.Text;
+
+                try
+                {
+                    CDBCommand.Connection = mainconnection;
+                    CDBCommand.CommandText = $"CREATE DATABASE {cdbname}";
+
+                    var execute = CDBCommand.ExecuteNonQuery();
+
+                    MessageBox.Show($"Database {cdbname} created.");
+                    _DATABASE.Clear();
+                }
+                catch (MySqlException ex)
+                {
+                    MessageBox.Show($"ERROR CREATING DATABASE. {ex.Message}");
+                }
+            };
+
+            CDBbackbutton.Click += (sender, e) =>
+            {
+                Connect.Visible = true;
+                CreateDatabase.Visible = true;
+                CDBbackbutton.Visible = false;
+                createdatabase.Visible = false;
+                _DATABASE.Visible = false;
+            };
+
+            this.Controls.Add(_DATABASE);
+            this.Controls.Add(createdatabase);
+            this.Controls.Add(CDBbackbutton);
+        }
+
+        private void CreateTable_Click(object sender, EventArgs e)
+        {
+            while(mainconnection == null)
+            {
+                MessageBox.Show("Connection has not been established. Connect to MYSQL using your login information first.");
+                return;
+            }
+
+            HideButtons();
+            TextBox _TABLE = new()
+            {
+                PlaceholderText = "Table Name",
+                Location = new(271, 45),
+                Width = 200,
+                ForeColor = Color.Red
+            };
+
+            TextBox _DBNAME = new()
+            {
+                PlaceholderText = "Database Name",
+                Location = new(271, 91),
+                Width = 200,
+                ForeColor = Color.Red
+            };
+
+            Button CTBackbutton = new()
+            {
+                Text = "Back",
+                Location = new(271, 183),
+                Width = 100
+            };
+
+            Button createtable = new()
+            {
+                Text = "Create",
+                Location = new(372, 183),
+                Width = 100
+            };
+
+            this.Controls.Add(_TABLE);
+            this.Controls.Add(CTBackbutton);
+            this.Controls.Add(createtable);
+        }
+    }
+}
